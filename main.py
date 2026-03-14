494890694a2cb04a22b6adf3d2aaa9890c880e4e
Babatunde Mattieu <bbmattieu9@gmail.com>
2026-03-14 15:24:44 +0100

    [Add] main.py and requirement.txt file to install fastapi & uvicorn

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/")
+async def home():
+    return {"message": "How fast is FastAPI?"}
+
+@app.get("/greet")
+def greet():
+    return {"message": "Hey Mattieu Guten Tag"}
+
+@app.get("/greet/{name}")
+def greet_name(name: str):
+    return {"message": f"Hello {name}"}
+
